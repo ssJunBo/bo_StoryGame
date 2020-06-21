63a925026a54c61b6e29c2f9b07f73cfe5ef07ed
ssJunBo <634052666@qq.com>
2020-06-21 11:44:13 +0800

    6.21修改

---
+++ Assets/Scripts/AssetBundleManager.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class AssetBundleManager : Singleton<AssetBundleManager>
+{
+
+    //资源关系依赖配表 可以根据crc来找到对应的资源块
+    protected Dictionary<uint, ResouceItem> m_ResouceItemDic = new Dictionary<uint, ResouceItem>();
+    //储存已加载的ab包，key为crc
+    protected Dictionary<uint, AssetBundleItem> m_AssetBundleItemDic = new Dictionary<uint, AssetBundleItem>();
+    //AssetBundleItem类对象池
+    protected ClassObjectPool<AssetBundleItem> m_AssetBundleItemPool = ObjectManager.Instance.GetOrCreateClassPool<AssetBundleItem>(500);
+
+    /// <summary>
+    /// 加载ab配置表
+    /// </summary>
+    /// <returns></returns>
+    public bool LoadAssetBundleConfig()
+    {
+        m_ResouceItemDic.Clear();
+        string configPath = Application.streamingAssetsPath + "/assetbundleconfig";
+        AssetBundle configAB = AssetBundle.LoadFromFile(configPath);
+        TextAsset textAsset = configAB.LoadAsset<TextAsset>("assetbundleconfig");
+        if (textAsset == null)
+        {
+            Debug.LogError("AssetBundleConfig is no exist !");
+            return false;
+        }
+        //解析 反序列化
+        MemoryStream stream = new MemoryStream(textAsset.bytes);
+        BinaryFormatter bf = new BinaryFormatter();
+        AssetBundleConfig config = (AssetBundleConfig)bf.Deserialize(stream);
+        stream.Close();
+
+        for (int i = 0; i < config.ABList.Count; i++)
+        {
+            ABBase abBase = config.ABList[i];
+            ResouceItem item = new ResouceItem();
+            item.m_Crc = abBase.Crc;
+            item.m_AssetName = abBase.AssetName;
+            item.m_ABName = abBase.ABName;
+            item.m_DependAssetBundle = abBase.ABDependce;
+            if (m_ResouceItemDic.ContainsKey(item.m_Crc))
+            {
+                Debug.LogError("重复的CRC：资源名：" + item.m_ABName + " ab包名" + item.m_ABName);
+            }
+            else
+            {
+                m_ResouceItemDic.Add(item.m_Crc, item);
+            }
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// 根据路径的crc加载中间类ResouceItem
+    /// </summary>
+    /// <param name="crc"></param>
+    /// <returns></returns>
+    public ResouceItem LoadResouceAssetBundle(uint crc)
+    {
+        ResouceItem item = null;
+        if (!m_ResouceItemDic.TryGetValue(crc, out item) || item == null)
+        {
+            Debug.LogError(string.Format("LoadResouceAssetBundle error : can not find crc {0} in AssetBundleConfig", crc.ToString()));
+            return item;
+        }
+        if (item.m_AssetBundle != null)
+        {
+            return item;
+        }
+        item.m_AssetBundle = LoadAssetBundle(item.m_ABName);
+        if (item.m_DependAssetBundle != null)
+        {
+            for (int i = 0; i < item.m_DependAssetBundle.Count; i++)
+            {
+                LoadAssetBundle(item.m_DependAssetBundle[i]);
+            }
+        }
+        return item;
+    }
+
+    /// <summary>
+    /// 加载单个assetbundle根据名字
+    /// </summary>
+    /// <param name="abName"></param>
+    /// <returns></returns>
+    private AssetBundle LoadAssetBundle(string abName)
+    {
+        AssetBundleItem item = null;
+        uint crc = CRC32.GetCRC32(abName);
+
+        if (!m_AssetBundleItemDic.TryGetValue(crc, out item))
+        {
+            AssetBundle assetBundle = null;
+            string fullPath = Application.streamingAssetsPath + "/" + abName;
+            if (File.Exists(fullPath))
+            {
+                assetBundle = AssetBundle.LoadFromFile(fullPath);
+            }
+            if (assetBundle == null)
+            {
+                Debug.LogError(" Load AssetBundle Errot : " + fullPath);
+            }
+
+            item = m_AssetBundleItemPool.Spawn(true);
+            item.assetBundle = assetBundle;
+            item.RefCount++;
+            m_AssetBundleItemDic.Add(crc, item);
+        }
+        else
+        {
+            item.RefCount++;
+        }
+
+        return item.assetBundle;
+    }
+}
+
+public class AssetBundleItem
+{
+    public AssetBundle assetBundle = null;
+    public int RefCount;
+
+    public void Rest()
+    {
+        assetBundle = null;
+        RefCount = 0;
+    }
+}
+public class ResouceItem
+{
+    //资源路径的crc
+    public uint m_Crc = 0;
+    //该资源的文件名
+    public string m_AssetName = string.Empty;
+    //该资源所在的AssetBundleName
+    public string m_ABName = string.Empty;
+    //该资源所依赖的AssetBundle
+    public List<string> m_DependAssetBundle = null;
+    //该资源加载完的AB包
+    public AssetBundle m_AssetBundle = null;
+}
---
+++ Assets/Scripts/AssetBundleTool/AssetBundleTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetBundleTest : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void TestLoadAB() 
+	{
+	 
+	}
+}
